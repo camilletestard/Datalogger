97f423115e5514a8aa538c1e932edb745024826c
camilletestard <32043468+camilletestard@users.noreply.github.com>
2021-12-06 08:32:05 -0500

    minor svm changes

--- Linear decoding/log_SVM.m
+++ Linear decoding/log_SVM.m
@@ -4,7 +4,7 @@
 %% Load data
 
 %Set path
-is_mac = 0;
+is_mac = 1;
 if is_mac
     cd('~/Dropbox (Penn)/Datalogger/Deuteron_Data_Backup/Ready to analyze output/')
 else
@@ -19,9 +19,11 @@ else
 end
 savePath = uigetdir('', 'Please select the result directory');
 
+clearvars -except savePath filePath
+
 %Set temporal resolution
-temp = 1; temp_resolution = 1;
-for temp_resolution = [1, 5, 10, 100] %1sec, 500msec, 100msec, 10msec
+temp = 1; temp_resolution = 1/15;
+for temp_resolution = [1/15, 1/10, 1/5, 1/2, 1, 2, 5, 10, 100] %1sec, 500msec, 100msec, 10msec
     %temp_resolution = [1/5, 1/2, 1, 5, 10] %5sec, 2sec, 1sec,500msec, 100msec
     %1 for second resolution, 10 for 100msec resolution, 100 for 10msec resolution, 1000 for msec resolution. etc.
     %0.1 for 10sec resolution, 1/5 for 5sec resolution
@@ -57,7 +59,7 @@ for temp_resolution = [1, 5, 10, 100] %1sec, 500msec, 100msec, 10msec
         % behav = behav_freq_table(behav_freq_table(:,2)>=min_occurrences,1);%[3,4,5,6,7,8,13,14,15,16];
         % behav = behav(behav~=find(matches(behav_categ,'Proximity')));%excluding proximity which is a source of confusion.
         % behav = behav(behav~=find(matches(behav_categ,'Scratch')));%excluding scratch which is a source of confusion.
-        behav = [3:8,17]; %[1:6,9:11,16,17]; %manually select behaviors of interest
+        behav = [5:6]; %[1:6,9:11,16,17]; %manually select behaviors of interest
         behavs_eval = behav_categ(behav);
 
         idx = find(ismember(behavior_labels,behav)); %find the indices of the behaviors considered
