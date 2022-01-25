db2e68d3902caf1e71865e85e27bdd8f14f0598e
camilletestard <camille.testard94@gmail.com>
2022-01-25 09:22:46 -0500

    Update NumUnitsPlot.R

--- Single neuron/NumUnitsPlot.R
+++ Single neuron/NumUnitsPlot.R
@@ -10,3 +10,12 @@ ggplot(data = data, aes(x=date, y=num.units, color = monkey))+
   theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1))+
   ylim(200, 350)+ylab('Number of units')
 ggsave('Number of units for all sessions.png')
+
+setwd(paste('~/Dropbox (Penn)/Datalogger/Results/', sessions[s],"/",sep=""))
+ggplot(data = data, aes(x=date, y=num.units.TEO))+
+  geom_point(size = 3, color='red')+ geom_point(aes(x=date, y=num.units.vlPFC), size = 3, color='blue')+
+  theme_classic(base_size=16)+
+  facet_wrap(~monkey)+
+  theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1))+
+  ylim(100, 200)+ylab('Number of units')
+ggsave('Number of units split by area.png')
