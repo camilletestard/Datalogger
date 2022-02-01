118ccdf227306e4c27cc562d3a6f1ed24fa23911
camilletestard <32043468+camilletestard@users.noreply.github.com>
2022-02-01 15:58:14 -0500

    Update preprocessing of ME

--- Motion energy/preprocess_ME.R
+++ Motion energy/preprocess_ME.R
@@ -23,14 +23,7 @@ if (diff_vids <0){
   ME_left[(length(ME_left)+1):(length(ME_left)+abs(diff_vids))]=0
 }
 
-MotionEnergy = cbind(ME_left, ME_right)
-
-mac =0
-if (mac == 1) { home = '~'} else
-{home = 'C:/Users/GENERAL'}
-
-setwd(paste(home,'/Dropbox (Penn)/Datalogger/Deuteron_Data_Backup/Ready to analyze output/', sep="" ))
-write.csv(MotionEnergy, 'Amos_2021-07-29/ME.csv',row.names=F)
+MotionEnergy = data.frame(cbind(ME_left, ME_right))
 
 # #Visualize ME
 # plot(1:3000, ME_right[1:3000], type = "S", frame = FALSE, 
@@ -43,9 +36,28 @@ length_recording = log[nrow(log),"Time"]/1000
 #deuteron log and "unit rasters". That is because in the script "log_read_sorted_NEX_file_CombinedChannels.m"
 #Unit_rasters is initialized by the line: Unit_rasters(unit,:) = zeros(1,round(length_recording*temp_resolution)+1);
 #round and +1 can lead to up to 2sec difference with the length of recording from the log.
+time_before_cameraSync = log[2,"Time"]/1000
+time_after_cameraSync = log[nrow(log),"Time"]/1000 - log[nrow(log)-1,"Time"]/1000
 
-#Get video length in sec
+#Estimate frame rate
 length_video = (log[nrow(log)-1,"Time"] - log[2,"Time"])/1000
-frame_rate = 29.973
+frame_rate = 29.971
 expected_num_frames = frame_rate*length_video
 diff_frames = abs(expected_num_frames-length(ME_right))
+
+#Pad ME with 0 in the extra recording spots
+total_frames_needed = round(log[nrow(log),"Time"]/1000*frame_rate)
+numFrames_before_cameraSync = round(time_before_cameraSync*frame_rate)
+numFrames_after_cameraSync = round(time_after_cameraSync*frame_rate)
+zero_pad_before = data.frame(matrix(data = 0, nrow = numFrames_before_cameraSync, ncol = 2)); names(zero_pad_before) = c("ME_left","ME_right")
+zero_pad_after = data.frame(matrix(data = 0, nrow = numFrames_after_cameraSync, ncol = 2)); names(zero_pad_after) = c("ME_left","ME_right")
+
+ME_final = rbind(zero_pad_before, MotionEnergy, zero_pad_after)
+
+#Save ME file
+mac =0
+if (mac == 1) { home = '~'} else
+{home = 'C:/Users/GENERAL'}
+
+setwd(paste(home,'/Dropbox (Penn)/Datalogger/Deuteron_Data_Backup/Ready to analyze output/', sep="" ))
+write.csv(ME_final, 'Amos_2021-07-29/ME.csv',row.names=F)
